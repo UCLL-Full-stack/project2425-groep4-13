7795620121ba99bd6d67960fcd90141846726df7
JustJNow <146000339+JustJNow@users.noreply.github.com>
2024-11-02 17:59:04 +0100

    added family domain object

---
+++ back-end/model/family.ts
@@ -0,0 +1,43 @@
+import { User } from "./user";
+
+export class Family {
+    private name: string;
+    private members: User[];
+
+    constructor(family: {
+        name: string;
+        members: User[];
+    }) {
+        this.validate(family);
+
+        this.name = family.name;
+        this.members = family.members;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getMembers(): User[] {
+        return this.members;
+    }
+
+    validate(family: {
+        name: string;
+        members: User[];
+    }) {
+        if (!family.name?.trim()) {
+            throw new Error('Name is required');
+        }
+        if (!family.members) {
+            throw new Error('Members are required');
+        }
+    }
+
+    equals(family: Family): boolean {
+        return (
+            this.name === family.getName() &&
+            this.members === family.getMembers()
+        );
+    }
+}
